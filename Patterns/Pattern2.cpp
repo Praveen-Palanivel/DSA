836827d0f60fc92df82556bd4620e48d45dbaf8f
Praveen-Palanivel <prvp90@gmail.com>
2025-01-31 18:18:11 +0530

    Right_Angled_Triangle_Star

---
+++ Patterns/Pattern2.cpp
@@ -0,0 +1,20 @@
+//Program for printing right-angled triangle star pattern
+
+#include <iostream>
+using namespace std;
+
+void Pattern(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    Pattern(n);
+    return 0;
+}
